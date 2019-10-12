dd87165c2db3309badea5a8da2a4f5b3a0279cb9
discodogg <759921582@qq.com>
2019-10-12 21:34:48 +0800

    fix：修改了订单列表页的功能逻辑代码，以及删除total属性

--- snackmall/apps/sm_info/static/info/js/my_orders.js
+++ snackmall/apps/sm_info/static/info/js/my_orders.js
@@ -4,11 +4,9 @@ var app = new Vue({
 	data: {
 		return_url : '/index/',
 		is_show: true,
-		id: '13068640035',
 		orderList: [{
 			status: '已接单',
 			dt: '2019-10-04 12:21',
-			total: 0,
 			shopList: {
 				list: [{
 						src: "shop/img/1.jpg",
@@ -40,7 +38,6 @@ var app = new Vue({
 		}, {
 			status: '已取消',
 			dt: '2019-10-04 12:21',
-			total: 0,
 			shopList: {
 				list: [{
 						src: "shop/img/1.jpg",
@@ -72,52 +69,94 @@ var app = new Vue({
 		}],
 
 
-
-
 	},
 	methods: {
 		toReturn()
 		{
 			window.location.href = this.return_url;
 		},
-		more_order() {
-			let that = this;
-			page++;
-			console.log(page);
+		
+		//传当前所需要的page信息，返回后端服务器传来的一个对象
+		get_orderList( page ){
 			xmlHttp = new XMLHttpRequest();
 			//设置请求的超时时间
 			xmlHttp.timeout = 3000;
-			xmlHttp.open("GET", "" + 'id=' + this.id + '&page=' + page);
+			xmlHttp.open("POST", "");
 			xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			//只需要发送用户id？
-			xmlHttp.send();
+			xmlHttp.send("page="+page);
+			
 			xmlHttp.onreadystatechange = function() {
 				if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-					//请求返回一个新的订单对象数组
-					// {
-					// 	result: 1,
-					// 	list:[]
-					// }
 					var json = JSON.parse(xmlHttp.responseText);
-					if (json.result == 1) { //如果result 返回值是true。则正确收到
-						for (let i = 0; i < json.list.length; i++) {
-							this.orderList.push(json.list[i]);
-						}
-					} else {
-						that.is_show = false;
+					return json;
+				}
+				else{
+					if(xmlHttp.status != 200){ //此处仍会执行多次！！！ readyState为2.3.4，时触发3次
+					alert("false");
+					return false;		
 					}
-				} else {
-						if(xmlHttp.status != 200){ //此处仍会执行多次！！！ readyState为2.3.4，时触发3次
-						alert("false");
-						return false;		
-						}
 				}
 			};
-			//请求超时的回调函数
 			xmlHttp.ontimeout = function(){
 				alert("请求超时，请重试");
+				return false;
 			}
-		}
+		},
+		// more_order() {
+		// 	let that = this;
+		// 	page++;
+		// 	console.log(page);
+		// 	xmlHttp = new XMLHttpRequest();
+		// 	//设置请求的超时时间
+		// 	xmlHttp.timeout = 3000;
+		// 	xmlHttp.open("GET", "" + 'id=' + this.id + '&page=' + page);
+		// 	xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		// 	//只需要发送用户id？
+		// 	xmlHttp.send();
+		// 	xmlHttp.onreadystatechange = function() {
+		// 		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+		// 			//请求返回一个新的订单对象数组
+		// 			// {
+		// 			// 	result: 1,
+		// 			// 	list:[]
+		// 			// }
+		// 			var json = JSON.parse(xmlHttp.responseText);
+		// 			if (json.result == 1) { //如果result 返回值是true。则正确收到
+		// 				for (let i = 0; i < json.list.length; i++) {
+		// 					this.orderList.push(json.list[i]);
+		// 				}
+		// 			} else {
+		// 				that.is_show = false;
+		// 			}
+		// 		} else {
+		// 				if(xmlHttp.status != 200){ //此处仍会执行多次！！！ readyState为2.3.4，时触发3次
+		// 				alert("false");
+		// 				return false;		
+		// 				}
+		// 		}
+		// 	};
+		// 	//请求超时的回调函数
+		// 	xmlHttp.ontimeout = function(){
+		// 		alert("请求超时，请重试");
+		// 	}
+		// },
+		
+		//点击加载更多按钮，请求更多订单信息
+		more_order() {
+			page++;
+			var json = get_orderList( page );
+			if(json.result){
+				for (let i = 0; i < json.list.length; i++) {
+					this.orderList.push(json.list[i]);
+				}
+			}else{
+				this.is_show = false;
+			}
+		},
+		
+		linkTo( index ){
+			window.location.href=" "+"?id=" + this.orderList[index].id
+		},
 
 	}
 
