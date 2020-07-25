9adc6dc566dc4a30e9d015703dcbbba30efaa511
邹银涛 <v_yintaozou@wesure.cn>
2020-07-25 17:39:43 +0800

    fix:修复版的SnackMall

--- snackmall/snackmall/settings.py
+++ snackmall/snackmall/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'e#d&4*o4-yit_p(yi46ws!srh!5_7xj^6c!u(wi&9@#k*vb!6$'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -81,27 +79,37 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'snackmall.wsgi.application'
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "CONNECTION_POOL_KWARGS": {"max_connections": 100}
+            # "PASSWORD": "密码",
+        }
+    }
+}
 
+WSGI_APPLICATION = 'snackmall.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    'default' : {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME' : 'snackmall',
-        'USER' : 'snackmall',
-        'PASSWORD' : 'zouyintao626',
-        'HOST' : '127.0.0.1',
-        'PORT' : 5432
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default' : {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME' : 'snackmall',
+    #     'USER' : 'snackmall',
+    #     'PASSWORD' : 'zouyintao626',
+    #     'HOST' : '127.0.0.1',
+    #     'PORT' : 5432
+    # }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -120,7 +128,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
